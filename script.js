2b326c4c716dde08c89976532bcee0ef6d73d695
abenezertesfaye <abenezertesfaye92@gmail.com>
2023-06-07 15:54:01 -0700

    basic two player mode

--- script.js
+++ script.js
@@ -1,40 +1,131 @@
-const moves = document.querySelectorAll('.card');
-const gameMode = document.querySelector('.game-mode');
-const turn = document.querySelector('.turn');
+let moves = document.querySelectorAll('.card');
+let select = document.getElementById('play-mode');
+let gameMode = document.querySelector('.game-mode');
+let turn = document.querySelector('.turn');
+
+const restart = document.getElementById('restart');
+
+let humanPlayer = "X";
+let aiPlayer = "O";
+let currentPlayer = humanPlayer;
+let gameActive = true;
+let positions = ['','','','','','','','',''];
 
 
-const select = document.getElementById('play-mode');
-select.addEventListener("change", () => {
-		let val = select.options[select.selectedIndex].value.toUpperCase();
-		gameMode.innerHTML = `${val} MODE`;
-		gameMode.classList.remove("hidden");
-		gameMode.classList.add("show");
-		let count = 0;
-		let player1 = "X";
-		let player2 = "O";
-		moves.forEach(btn => {
-			btn.addEventListener("click", (e) => {
-				const target = e.target;
-				const move = target.firstChild;
-				move.innerHTML = player1;
-			});
+const Play = () => {
+
+	const playMode = () => {
+			select.addEventListener("change", () => {
+				let val = select.options[select.selectedIndex].value;
+				gameMode.innerHTML = `${val} MODE`;
+				gameMode.classList.remove("hidden");
+				gameMode.classList.add("show");
+
+				turn.textContent = `It's ${currentPlayer}'s turn!`;
 		});
+	}
+
+	const playerTurn = () => {
+		turn.textContent = `It's ${currentPlayer}'s turn!`;
+	}
+
+	const makeAiMove = () => {
+		const emptyCells = [];
+		for (let i = 0; i < positions.length; i++) {
+		  if (positions[i] === '') {
+			emptyCells.push(i);
+		  }
+		}
+		const randomIndex = Math.floor(Math.random() * emptyCells.length);
+		const aiMove = emptyCells[randomIndex];
+		positions[aiMove] = aiPlayer;
+		moves[aiMove].textContent = aiPlayer;
+	  }
+	  
+
+	const handleClick = (e) => {
+		const box = e.target;
+		const position = box.dataset.id;
 		
-});
+		if(positions[position] !== '' || !gameActive){
+			return
+		}
+		positions[position] = currentPlayer;
+		box.textContent = currentPlayer;
+		box.classList.add("center");
 
+		handleWin();
 
-		// 	if (val === "TWO-PLAYER") {
-		// 		if(count % 2 == 0){
-		// 			turn.innerHTML = `it's ${player1}'s turn`;
-		// 		} else {
-		// 			turn.innerHTML = `it's ${player2}'s turn`;
-		// 		}
+		// if (gameActive && currentPlayer === aiPlayer) {
+		// 	setTimeout(makeAiMove, 500); // Delay AI move for better user 
+		// 	currentPlayer = currentPlayer === "X" ? "O" : "X";
+		// 	playerTurn();
+			
 		//   }
 
-// moves.forEach(btn => {
-// 	btn.addEventListener("click", (e) => {
-// 		const target = e.target;
-// 		console.log(target);
-// 		// target.classList.contains("selected") ? target.classList.remove("selected"): target.classList.add("selected");
-// 	})
-// })
+	}
+
+	const handleWin = () => {
+		const winConditions = [
+			[0,1,2], [3,4,5], [6,7,8], //row
+			[0,3,6], [1,4,7], [2,5,8], //column
+			[0,4,8], [2,4,6] //diagonal
+		];
+
+		let won = false;
+
+		for (let i = 0; i < winConditions.length; i++){
+			const [a,b,c] = winConditions[i];
+
+			if (positions[a] !== '' && positions[a] == positions[b] && positions[a] == positions[c]){
+				won = true;
+				break;
+			}
+		}
+
+		if (won){
+			handleAnnouncement(`${currentPlayer} has Won!`);
+			gameActive = false;
+			// reset();
+			return;
+		}
+		if (!positions.includes('')){
+			handleAnnouncement("IT's Draw!")
+			gameActive = false;
+			// reset();
+			return;
+		}
+
+		currentPlayer = currentPlayer === "X" ? "O" : "X";
+		playerTurn();
+	}
+
+	const handleAnnouncement = (message) => {
+		alert(message);
+	}
+
+	const reset = () => {
+		gameMode.innerHTML = "";
+		gameActive = true;
+		currentPlayer = "X";
+		positions = ['','','','','','','','',''];
+
+		moves.forEach(move => move.textContent ="");
+		
+	}
+
+	moves.forEach((move) => {
+		move.addEventListener("click", handleClick);
+	})
+
+	playMode();
+
+	restart.addEventListener("click", reset);
+
+	// return {handleClick, handleWin, playMode};
+}; 
+
+Play();
+// playMode();
+
+
