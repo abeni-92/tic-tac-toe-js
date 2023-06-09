4d5f3823311a3d98398c51d5b909bfb031e39a94
abenezertesfaye <abenezertesfaye92@gmail.com>
2023-06-09 16:50:51 -0700

    tic tac

--- script.js
+++ script.js
@@ -2,33 +2,44 @@ let moves = document.querySelectorAll('.card');
 let select = document.getElementById('play-mode');
 let gameMode = document.querySelector('.game-mode');
 let turn = document.querySelector('.turn');
+let end = document.getElementById('end');
 
 const restart = document.getElementById('restart');
 
-let humanPlayer = "X";
-let aiPlayer = "O";
-let currentPlayer = humanPlayer;
+let currentPlayer = "X";
+let aiPlayer; 
 let gameActive = true;
+let gameStart = false;
 let positions = ['','','','','','','','',''];
 
+const PlayMode = () => {
+	const Value = () => {
+		select.addEventListener("change", () => {
+			let val = select.options[select.selectedIndex].value;
+			gameMode.innerHTML = `${val} MODE`;
+			gameMode.classList.remove("hidden");
+			gameMode.classList.add("show");
+		
+			turn.textContent = `It's ${currentPlayer}'s turn!`;
+
+			if (val == "single-player"){
+				aiPlayer = "O";
+			} else {
+				aiPlayer = "";
+			}
+
+			gameStart = true;
+		});		
+  	 }
+
+   return {Value};
+}
 
 const Play = () => {
-
-	const playMode = () => {
-			select.addEventListener("change", () => {
-				let val = select.options[select.selectedIndex].value;
-				gameMode.innerHTML = `${val} MODE`;
-				gameMode.classList.remove("hidden");
-				gameMode.classList.add("show");
-
-				turn.textContent = `It's ${currentPlayer}'s turn!`;
-		});
-	}
-
-	const playerTurn = () => {
-		turn.textContent = `It's ${currentPlayer}'s turn!`;
-	}
-
+	
+	const {Value} = PlayMode();
+	Value();
+	
 	const makeAiMove = () => {
 		const emptyCells = [];
 		for (let i = 0; i < positions.length; i++) {
@@ -40,14 +51,15 @@ const Play = () => {
 		const aiMove = emptyCells[randomIndex];
 		positions[aiMove] = aiPlayer;
 		moves[aiMove].textContent = aiPlayer;
+		moves[aiMove].classList.add("center");
 	  }
 	  
-
 	const handleClick = (e) => {
+		
 		const box = e.target;
 		const position = box.dataset.id;
 		
-		if(positions[position] !== '' || !gameActive){
+		if(positions[position] !== '' || !gameActive || !gameStart){
 			return
 		}
 		positions[position] = currentPlayer;
@@ -55,14 +67,13 @@ const Play = () => {
 		box.classList.add("center");
 
 		handleWin();
+		playerTurn();
 
-		// if (gameActive && currentPlayer === aiPlayer) {
-		// 	setTimeout(makeAiMove, 500); // Delay AI move for better user 
-		// 	currentPlayer = currentPlayer === "X" ? "O" : "X";
-		// 	playerTurn();
-			
-		//   }
-
+		if (gameActive && aiPlayer !== "" && currentPlayer === aiPlayer) {
+			setTimeout(makeAiMove, 500); // Delay AI move for better user 
+			// currentPlayer = currentPlayer === "X" ? "O" : "X";
+			handleWin();
+		  }
 	}
 
 	const handleWin = () => {
@@ -71,7 +82,6 @@ const Play = () => {
 			[0,3,6], [1,4,7], [2,5,8], //column
 			[0,4,8], [2,4,6] //diagonal
 		];
-
 		let won = false;
 
 		for (let i = 0; i < winConditions.length; i++){
@@ -82,50 +92,54 @@ const Play = () => {
 				break;
 			}
 		}
-
 		if (won){
 			handleAnnouncement(`${currentPlayer} has Won!`);
 			gameActive = false;
-			// reset();
 			return;
 		}
 		if (!positions.includes('')){
 			handleAnnouncement("IT's Draw!")
 			gameActive = false;
-			// reset();
 			return;
 		}
-
-		currentPlayer = currentPlayer === "X" ? "O" : "X";
+		if (aiPlayer != "") {
+			currentPlayer = currentPlayer === "X" ? aiPlayer : "X";
+		} else {
+			currentPlayer = currentPlayer === "X" ? "O" : "X";
+		}
 		playerTurn();
 	}
 
 	const handleAnnouncement = (message) => {
-		alert(message);
+		end.textContent = message;
+		end.classList.add("message");
+		turn.classList.add("hidden");
+		gameMode.textContent = "";
 	}
-
 	const reset = () => {
 		gameMode.innerHTML = "";
+		end.textContent = "";
 		gameActive = true;
+		gameStart = false;
 		currentPlayer = "X";
 		positions = ['','','','','','','','',''];
-
+		select.value = "";
 		moves.forEach(move => move.textContent ="");
-		
+		// playerTurn();
 	}
+	const playerTurn = () => {
+		turn.textContent = `It's ${currentPlayer}'s turn!`;
+	}
+
 
 	moves.forEach((move) => {
 		move.addEventListener("click", handleClick);
 	})
 
-	playMode();
-
 	restart.addEventListener("click", reset);
 
-	// return {handleClick, handleWin, playMode};
+	return {handleClick, handleWin, playerTurn, Value};
 }; 
 
 Play();
-// playMode();
-
 
